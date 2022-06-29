9871c1a57ed79d63846e30e3e7ef139573287921
Jeremy Howard <info@fast.ai>
2022-06-29 00:43:47 +0000

    init

---
+++ pre-run.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+pushd ~
+rm -rf .config .fastai .jupyter .local .ssh .bash_history
+
+for p in .local .ssh .config .ipython .fastai .jupyter .conda .kaggle
+do
+	if [ ! -e /storage/cfg/$p ]; then
+		mkdir /storage/cfg/$p
+	fi
+	ln -s /storage/cfg/$p ~/
+done
+
+for p in .local .git-credentials .gitconfig .bash_history
+do
+	if [ ! -e /storage/cfg/$p ]; then
+		touch /storage/cfg/$p
+	fi
+	ln -s /storage/cfg/$p ~/
+done
+
+export PATH=~/.local/bin:~/.conda/bin/:$PATH
+export MAMBA_ROOT_PREFIX=~/.conda
+alias mamba='~/.conda/bin/micromamba -p .conda -c conda-forge '
+alias pipi='pip install --user '
+
