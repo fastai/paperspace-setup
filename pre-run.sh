bda0872681871a19f1e239586437667be52438e0
Jeremy Howard <github@jhoward.fastmail.fm>
2022-07-02 09:54:20 +1000

    Update pre-run.sh

--- pre-run.sh
+++ pre-run.sh
@@ -5,7 +5,11 @@ rm -rf .config .fastai .jupyter .local .ssh .bash_history .conda
 
 mkdir -p /storage/cfg
 
-for p in .local .ssh .config .ipython .fastai .jupyter .conda .kaggle
+if [ ! -e /storage/cfg/.conda ]; then
+        mamba create -yp /storage/cfg/.conda
+fi
+
+for p in .conda .local .ssh .config .ipython .fastai .jupyter .conda .kaggle
 do
 	if [ ! -e /storage/cfg/$p ]; then
 		mkdir /storage/cfg/$p
