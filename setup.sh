e11de362f458b796ef40669d71f44c5f90b86774
Ben Coman <git@ben.coman.com.au>
2022-07-05 11:47:51 +0000

    setup packages used by fastai course

--- setup.sh
+++ setup.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 
 mkdir /storage/cfg
-cp pre-run.sh /storage/
-cp .bash.local /storage/
+ln -sf `pwd`/pre-run.sh /storage/pre-run.sh
+ln -sf `pwd`/.bash.local /storage/
+
+./pre-run.sh
+source ./.bash.local
+shopt -s expand_aliases
+pipi -U fastai kaggle "timm>=0.6.2dev"
+mambai --yes -c conda-forge  universal-ctags  unzip  pv 
+
 echo install complete. please start a new instance
