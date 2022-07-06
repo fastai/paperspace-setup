d06662156353038e6b4713a329e2664e39201815
Ben Coman <git@ben.coman.com.au>
2022-07-06 09:46:58 +0000

    use absolute folders and run from anywhere

--- setup.sh
+++ setup.sh
@@ -1,13 +1,20 @@
 #!/usr/bin/env bash
 
-mkdir /storage/cfg
-ln -sf `pwd`/pre-run.sh /storage/pre-run.sh
-ln -sf `pwd`/.bash.local /storage/
+pushd $(dirname "$0")     # change to folder containing script
 
-./pre-run.sh
-source ./.bash.local
+mkdir -p /storage/cfg
+ln -sf `pwd`/pre-run.sh /storage/pre-run.sh
+ln -sf `pwd`/.bash.local /storage/.bash.local
+
+/storage/pre-run.sh
+source /storage/.bash.local
 shopt -s expand_aliases
+
 pipi -U fastai kaggle "timm>=0.6.2dev"
 mambai --yes -c conda-forge  universal-ctags  unzip  pv 
+mambai --yes -c fastai nvidia-ml-py3 
+
+popd   # return to original folder 
 
 echo install complete. please start a new instance
+
