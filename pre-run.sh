471f5cdd9ec52ad10c9a7119bf172f919c0a9db1
Lucas van Walstijn <maillucas@gmail.com>
2023-02-08 10:39:41 +0100

    Add .huggingface folder
    Add .huggingface folder to the list of folders to copy from /storage/cfg/ to /root/

--- pre-run.sh
+++ pre-run.sh
@@ -8,7 +8,7 @@ if [ ! -e /storage/cfg/.conda ]; then
         mamba create -yp /storage/cfg/.conda
 fi
 
-for p in .local .ssh .config .ipython .fastai .jupyter .conda .kaggle
+for p in .local .ssh .config .ipython .fastai .jupyter .conda .kaggle .huggingface
 do
         if [ ! -e /storage/cfg/$p ]; then
                 mkdir /storage/cfg/$p
